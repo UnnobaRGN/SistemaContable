2d62f70658afc6c9888cc736a9eb455e83170593
Ramiro1999 <58752200+Ramiro1999@users.noreply.github.com>
2020-08-28 22:37:16 -0300

    prueba1

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
-        primaryStage.setTitle("Prueba");
+        primaryStage.setTitle("Sistema Contable");
         primaryStage.setScene(new Scene(root, 329, 484));
         primaryStage.show();
     }
