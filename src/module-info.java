e4d332f65986a86dcf6d7188473ad6ee85a6f7ec
Ramiro1999 <58752200+Ramiro1999@users.noreply.github.com>
2020-08-29 19:35:50 -0300

    En el module info no anda la libreria de posgresql!

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+module SistemaContable {
+
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.sql;
+
+
+    opens sample;
+}
--- src/sample/LoginController.java
+++ src/sample/LoginController.java
@@ -1,5 +1,39 @@
 package sample;
 
-public class LoginController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class LoginController implements Initializable {
+
+    @FXML
+    private Label loginMessageLabel;
+
+    @FXML
+    private ImageView ImagenPrincipal;
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        File brandingFile = new File("Imagenes/Captura.PNG");
+        Image brandingImage = new Image(brandingFile.toURI().toString());
+        ImagenPrincipal.setImage(brandingImage);
+
+    }
+
+    public void loginButtonAction(ActionEvent event){
+        loginMessageLabel.setText("Trataste de loguearte");
+
+    }
 
 }
