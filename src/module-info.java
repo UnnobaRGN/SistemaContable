ddd4859410ea05f0a72ceaba232f079926deedf9
Ramiro1999 <58752200+Ramiro1999@users.noreply.github.com>
2020-09-04 19:49:44 -0300

    RGN 4/9/2020 v2

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+module SistemaContable {
+
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.sql;
+    requires org.postgresql.jdbc;
+
+    opens sample;
+}
