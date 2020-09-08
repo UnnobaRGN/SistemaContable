b873c0078cb16d270b3943455004addff8cf43ec
German <ger_quiroga@hotmail.com>
2020-09-08 19:31:50 -0300

    Prueba

---
+++ src/sample/PrincipalController.java
@@ -0,0 +1,33 @@
+package sample;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import java.io.File;
+import java.net.URL;
+import java.sql.*;
+import java.util.*;
+
+
+public class PrincipalController {
+
+    @FXML
+    private Button Boton1;
+
+    @FXML
+    private Button boton2;
+
+    private UsuarioLogeado u;
+
+    public void aCasa(ActionEvent event) {
+        System.out.println("1312312");
+    }
+
+}
