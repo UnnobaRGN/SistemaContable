61a3719fed5bb4edc7341ed524ac79bfb184f66b
Ramiro1999 <58752200+Ramiro1999@users.noreply.github.com>
2020-09-25 18:41:59 -0300

    RGN 25/9/2020

--- src/Controladores/PrincipalController.java
+++ src/Controladores/PrincipalController.java
@@ -2,21 +2,38 @@ package Controladores;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import Modelo.UsuarioLogeado;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+
 
 
 public class PrincipalController implements Initializable {
 
     @FXML
-    private Button Boton1;
+    private ImageView ImagenFondo = new ImageView();
 
     @FXML
-    private Button boton2;
+    private ImageView Imagen1 = new ImageView();
+
+    @FXML
+    private ImageView Imagen2 = new ImageView();
+
+    @FXML
+    private ImageView Imagen3 = new ImageView();
+
+    @FXML
+    private ImageView Imagen4 = new ImageView();
+
+    @FXML
+    private MenuItem AgregarCuenta = new MenuItem();
 
     private UsuarioLogeado u = UsuarioLogeado.getInstance();
 
@@ -25,7 +42,28 @@ public class PrincipalController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        Boton1.setVisible(u.getIdperfil() == 1);
+        File brandingFile = new File("Imagenes/2.jpg");
+        Image brandingImage = new Image(brandingFile.toURI().toString());
+        ImagenFondo.setImage(brandingImage);
+
+        File brandingFile1 = new File("Imagenes/3.jpg");
+        Image brandingImage1 = new Image(brandingFile1.toURI().toString());
+        Imagen1.setImage(brandingImage1);
+
+        File brandingFile2 = new File("Imagenes/3.jpg");
+        Image brandingImage2 = new Image(brandingFile2.toURI().toString());
+        Imagen2.setImage(brandingImage2);
+
+        File brandingFile3 = new File("Imagenes/3.jpg");
+        Image brandingImage3= new Image(brandingFile3.toURI().toString());
+        Imagen3.setImage(brandingImage3);
+
+        File brandingFile4 = new File("Imagenes/3.jpg");
+        Image brandingImage4 = new Image(brandingFile4.toURI().toString());
+        Imagen4.setImage(brandingImage4);
+
+
+        AgregarCuenta.setVisible(u.getIdperfil() == 1);
     }
 
 
@@ -35,5 +73,8 @@ public class PrincipalController implements Initializable {
         System.out.println("hola");
 
     }
+
+
+
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,12 +10,12 @@ import javafx.stage.StageStyle;
 
 public class Main extends Application {
     public static Stage primaryStage = null;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
 
         Parent root = FXMLLoader.load(getClass().getResource("/Vista/Login.fxml"));
         primaryStage.initStyle(StageStyle.UNDECORATED);
-
 
         primaryStage.setScene(new Scene(root, 329, 484));
         this.primaryStage = primaryStage;
