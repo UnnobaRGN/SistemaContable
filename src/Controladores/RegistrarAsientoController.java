815cfec29438bdecdfceca253219525e4d06d86e
Ramiro1999 <58752200+Ramiro1999@users.noreply.github.com>
2020-10-11 22:41:55 -0300

    Agrege cuentas a registrar asiento

--- src/Controladores/RegistrarAsientoController.java
+++ src/Controladores/RegistrarAsientoController.java
@@ -1,18 +1,23 @@
 package Controladores;
 
+import Modelo.Cuentas;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.DatePicker;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import sample.ConexionBD;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.ResourceBundle;
@@ -25,18 +30,23 @@ public class RegistrarAsientoController implements Initializable {
     @FXML
     private DatePicker FechaRegistrarAsiento = new DatePicker();
 
-
+    @FXML
+    private ComboBox CuentasSeleccion;
 
     @FXML
     private Button ButtonCancelar;
+
+    @FXML
+    private TextField Texto;
+
+    ObservableList<String> list = FXCollections.observableArrayList();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         File fileFondoRegistrarAsiento = new File("Imagenes/2.jpg");
         Image brandingFondoRegistrarAsiento = new Image(fileFondoRegistrarAsiento.toURI().toString());
         FondoRegistrarAsiento.setImage(brandingFondoRegistrarAsiento);
-
-
+        traerCuentasAcomboBox();
         ///////DatePicker FechaRegistrarAsiento = new DatePicker(LocalDate.now());
         ///////FechaRegistrarAsiento.setEditable(false);
 
@@ -48,4 +58,47 @@ public class RegistrarAsientoController implements Initializable {
     }
 
 
+    public void traerCuentasAcomboBox(){
+        ObservableList<String> list = FXCollections.observableArrayList();
+        this.CuentasSeleccion.getItems().clear();
+
+        try{
+            Connection conn = ConexionBD.getConnection();
+            Statement s = conn.createStatement();
+            String SQL = "SELECT * FROM cuenta WHERE recibe_saldo = 1";
+            ResultSet  rs =  s.executeQuery(SQL);
+
+            while (rs.next()){
+                list.add(rs.getString("cuenta"));
+            }
+
+            CuentasSeleccion.setItems(list);
+        }catch(Exception e){
+
+        }
+
+    }
+
+    public void guardarAsiento(ActionEvent actionEvent){
+        Connection conn = ConexionBD.getConnection();
+        String s = CuentasSeleccion.getSelectionModel().getSelectedItem().toString();
+        try{
+
+            String SQL = "SELECT * FROM cuenta WHERE cuenta= '"+ s + "'";
+            Statement statement = conn.createStatement();
+            ResultSet rs = statement.executeQuery(SQL);
+
+            System.out.println(rs.getString("codigo_cuenta"));
+            if(rs.next()){
+                Cuentas cuenta = new Cuentas(rs.getInt("codigo_cuenta"),rs.getString("cuenta"),rs.getInt("recibe_saldo"),rs.getString("idtipo"),rs.getInt("idcuenta"),rs.getInt("saldo_actual"));
+                System.out.println(cuenta.getCodigo_cuenta());
+            }
+
+        }catch(Exception e){
+
+        }
+
+
+
+    }
 }
