629b0e1c613c35775e10e9c6821f29c6eb8f19c1
German <ger_quiroga@hotmail.com>
2020-10-25 14:47:54 -0300

    dios no me sale

--- src/Controladores/LibroMayorController.java
+++ src/Controladores/LibroMayorController.java
@@ -19,6 +19,9 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ResourceBundle;
 
 public class LibroMayorController implements Initializable {
@@ -31,6 +34,9 @@ public class LibroMayorController implements Initializable {
 
     @FXML
     private Button LimpiarFiltro;
+
+    @FXML
+    private Button VerCuenta;
 
     @FXML
     private Button Salir;
@@ -62,6 +68,8 @@ public class LibroMayorController implements Initializable {
     @FXML
     private TableColumn<Cuenta_Asiento, Float> columnaSaldo;
 
+    ObservableList<Cuenta_Asiento> list = FXCollections.observableArrayList();
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -73,11 +81,95 @@ public class LibroMayorController implements Initializable {
 
     }
 
+    public void filtrarFechas(ActionEvent e){
+        if(fechaDesde.getValue() != null && fechaHasta.getValue() != null){
+            if(compararFechas(Date.valueOf(fechaDesde.getValue()), Date.valueOf(fechaHasta.getValue()))){
+
+                Date fechaDe = Date.valueOf(fechaDesde.getValue());
+                Date fechaHas = Date.valueOf(fechaHasta.getValue());
+
+                list = mostrarFiltraros(fechaDe, fechaHas);
+                TablaLibroMayor.setItems(list);
+            }else{
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Atencion!");
+                alert.setHeaderText("Por favor,");
+                alert.setContentText("Ingrese un año valido.");
+                alert.showAndWait();
+            }
+        }
+        else {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Atencion!");
+            alert.setHeaderText("Por favor,");
+            alert.setContentText("Ingrese ambas fechas para el filtrado.");
+            alert.showAndWait();
+        }
+
+    }
+
+
+    public ObservableList<Cuenta_Asiento> mostrarFiltraros(Date dd, Date dh){
+
+        Connection conn = ConexionBD.getConnection();
+        ObservableList<Cuenta_Asiento> list = FXCollections.observableArrayList();
+        String s = menuCuenta.getSelectionModel().getSelectedItem().toString();
+
+        try {
+
+            String SQL = "SELECT c.cuenta as cuenta, a.descripcion as descripcion, ca.debe as debe, ca.haber as haber,ca.saldo as saldo FROM cuenta c INNER JOIN cuenta_asiento ca ON c.idcuenta=ca.id_cuenta INNER JOIN asiento a ON a.idasiento=ca.id_asiento WHERE c.cuenta='"+ s +"'  AND  fecha BETWEEN '" + dd + "'AND'" + dh + "' ORDER BY a.fecha";
+            Statement statement = conn.createStatement();
+            ResultSet rs = statement.executeQuery(SQL);
+
+            while (rs.next()) {
+                Cuenta_Asiento c = new Cuenta_Asiento();
+                c.setDescrip(rs.getString("descripcion"));
+                c.setDebe(rs.getFloat("debe"));
+                c.setHaber(rs.getFloat("haber"));
+                c.setSaldo(rs.getFloat("saldo"));
+                list.add(c);
+            }
+
+        } catch (Exception e) {
+
+        }
+        return list;
+
+
+    }
+
+    public boolean compararFechas(Date dd, Date dh){
+        /*java.util.Date ahora = new java.util.Date(System.currentTimeMillis());
+        LocalDate desde = fechaDesde.getValue();
+        formateador.format(desde);
+        System.out.println(desde);
+        Date fechaHas = Date.valueOf(fechaHasta.getValue());
+
+        //java.util.Date desde = new java.util.Date(formateador.format(dd));
+        //java.util.Date hasta = new java.util.Date(formateador.format(dh));
+*/
+        LocalDate ahora = LocalDate.now();
+        Date a = Date.valueOf(ahora);
+        if (dh.after(dd)){//formateador.format(dd) < formateador.format(dh))){ //&& formateador.format(dh).equals(formateador.format(ahora))){
+            if(dh.before(a)){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            return false;
+        }
+
+    }
+
     public void mostrarCuenta(ActionEvent a){
         muestraCuenta.setDisable(true);
         muestraCuenta.setText(menuCuenta.getSelectionModel().getSelectedItem().toString());
         mostrarDatos();
     }
+
     public void limpiarFiltro(){
         TablaLibroMayor.setItems(null);
         fechaDesde.setValue(null);
@@ -147,7 +239,6 @@ public class LibroMayorController implements Initializable {
         columnaSaldo.setCellValueFactory(new PropertyValueFactory<Cuenta_Asiento, Float>("saldo"));
 
         TablaLibroMayor.setItems(mostrarDatosLibro());
-
 
     }
 
--- src/Controladores/PrincipalController.java
+++ src/Controladores/PrincipalController.java
@@ -21,7 +21,6 @@ import java.util.ResourceBundle;
 
 
 
-
 public class PrincipalController implements Initializable {
 
     @FXML
