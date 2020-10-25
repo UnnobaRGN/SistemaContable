653b6b346c4aa638d20701e8c485f8caad1f0761
German <ger_quiroga@hotmail.com>
2020-10-25 19:02:54 -0300

    va2

--- src/Controladores/DatosEmpresaController.java
+++ src/Controladores/DatosEmpresaController.java
@@ -1,5 +1,6 @@
 package Controladores;
 
+import Modelo.Empresa;
 import Modelo.UsuarioLogeado;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -11,6 +12,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -20,7 +22,7 @@ import java.util.ResourceBundle;
 public class DatosEmpresaController implements Initializable {
 
     @FXML
-    private ImageView imagenFondo;
+    private ImageView ImagenFondo;
 
     @FXML
     private TextField textRazon;
@@ -38,7 +40,7 @@ public class DatosEmpresaController implements Initializable {
     private TextField textProvincia;
 
     @FXML
-    private ComboBox<String> comboBoxIva;
+    private TextField textIva;
 
     @FXML
     private TextField textEmail;
@@ -57,36 +59,60 @@ public class DatosEmpresaController implements Initializable {
 
     private UsuarioLogeado u = UsuarioLogeado.getInstance();
 
+    private Empresa empresa = new Empresa();
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
         File brandingFile = new File("Imagenes/2.jpg");
         Image brandingImage = new Image(brandingFile.toURI().toString());
-        imagenFondo.setImage(brandingImage);
+        ImagenFondo.setImage(brandingImage);
 
         botonActualizar.setVisible(u.getIdperfil() == 1);
 
-        llenarIva();
+        mostrarDatos();
+        noModifica();
 
     }
 
-    public void llenarIva(){
-
-        ObservableList<String> list = FXCollections.observableArrayList("Responsable Inscripto","Monotributista");
-        comboBoxIva.setItems(list);
+    public void noModifica(){
+        if(u.getIdperfil() == 0){
+        textTelefono.setDisable(true);
+        textRazon.setDisable(true);
+        textProvincia.setDisable(true);
+        textLocalidad.setDisable(true);
+        textEmail.setDisable(true);
+        textDireccion.setDisable(true);
+        textCodigo.setDisable(true);
+        textCuit.setDisable(true);
+        textIva.setDisable(true);}
 
     }
 
     public void actualizarDatos(ActionEvent e){
+        
+        empresa.setCodigoPostal(textCodigo.getText());
+        empresa.setCUIT(textCuit.getText());
+        empresa.setDireccion(textDireccion.getText());
+        empresa.setEmail(textEmail.getText());
+        empresa.setIva(textIva.getText());
+        empresa.setLocalidad(textLocalidad.getText());
+        empresa.setProvincia(textProvincia.getText());
+        empresa.setRazonSocial(textRazon.getText());
+        empresa.setTelefono(textTelefono.getText());
 
-        textCodigo.setText(textCodigo.getText());
-        textCuit.setText(textCuit.getText());
-        textDireccion.setText(textDireccion.getText());
-        textEmail.setText(textEmail.getText());
-        textLocalidad.setText(textLocalidad.getText());
-        textProvincia.setText(textProvincia.getText());
-        textRazon.setText(textRazon.getText());
-        textTelefono.setText(textTelefono.getText());
+    }
+
+    public void mostrarDatos(){
+
+        textCuit.setText(empresa.getCUIT());
+        textCodigo.setText(empresa.getCodigoPostal());
+        textDireccion.setText(empresa.getDireccion());
+        textEmail.setText(empresa.getEmail());
+        textLocalidad.setText(empresa.getLocalidad());
+        textProvincia.setText(empresa.getProvincia());
+        textRazon.setText(empresa.getRazonSocial());
+        textTelefono.setText(empresa.getTelefono());
+        textIva.setText(empresa.getIva());
 
     }
 
---
+++ src/Modelo/Empresa.java
@@ -0,0 +1,88 @@
+package Modelo;
+
+public class Empresa {
+
+    private String RazonSocial;
+    private String Email;
+    private String Direccion;
+    private String CUIT;
+    private String Localidad;
+    private String Provincia;
+    private String Iva;
+    private String CodigoPostal;
+    private String Telefono;
+
+    public void Empresa(){};
+
+    public String getRazonSocial() {
+        return RazonSocial;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        RazonSocial = razonSocial;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        Direccion = direccion;
+    }
+
+    public String getCUIT() {
+        return CUIT;
+    }
+
+    public void setCUIT(String CUIT) {
+        this.CUIT = CUIT;
+    }
+
+    public String getLocalidad() {
+        return Localidad;
+    }
+
+    public void setLocalidad(String localidad) {
+        Localidad = localidad;
+    }
+
+    public String getProvincia() {
+        return Provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        Provincia = provincia;
+    }
+
+    public String getIva() {
+        return Iva;
+    }
+
+    public void setIva(String iva) {
+        Iva = iva;
+    }
+
+    public String getCodigoPostal() {
+        return CodigoPostal;
+    }
+
+    public void setCodigoPostal(String codigoPostal) {
+        CodigoPostal = codigoPostal;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        Telefono = telefono;
+    }
+}
